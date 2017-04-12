8a9eaf3497ad6da00c2b8e439c1d6dcad64904d0
Hydrozz <pmtanguy@gmail.com>
2017-04-12 17:20:08 +0200

    projet test

---
+++ hydrozz/src/main/java/hydrozz/App.java
@@ -0,0 +1,13 @@
+package hydrozz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
